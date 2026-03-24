fbfb97311f98d18ba657400400f837a293f676a4
thumama610 <thumamaalrawwad610@gmail.com>
2026-03-24 22:34:21 +0300

    added monitoring server - terraform

--- terraform/main-worker1.tf
+++ terraform/main-worker1.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "worker1-server" {
   ami                    = "ami-0b6c6ebed2801a5cb"      
-  instance_type          = "t3.medium"
+  instance_type          = "t3.large"
   key_name               = "private-key"             
   vpc_security_group_ids = [aws_security_group.k8s-servers-sg.id]
-  user_data              = templatefile("./install-k8s.sh", {})
+  user_data              = templatefile("./install-k8s-worker.sh", {})
   subnet_id              = data.aws_subnet.default_az.id
   associate_public_ip_address = true
 
@@ -12,6 +12,6 @@ resource "aws_instance" "worker1-server" {
   }
 
   root_block_device {
-    volume_size = 8
+    volume_size = 20
   }
 }
